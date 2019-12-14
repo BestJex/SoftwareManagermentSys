2f5a82bb09e4cda1b33ca1870ea59df0012d4dbe
Your Name <you@example.com>
2019-12-15 00:32:04 +0800

    调试前段框架的布局

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -1,50 +1,52 @@
 <template>
-  <el-container style="height: 100vh">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu :default-openeds="['1', '3']">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-message"></i>文件管理
-          </template>
-          <el-menu-item-group>
-            <template slot="title">Mc</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <template slot="title">An</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <template slot="title">Ac</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <span id="title">内网版本管理系统</span>
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>Admin</span>
+    </el-header>
+    <el-container style="height: 100vh">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '3']">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-s-order"></i>文件管理
+            </template>
+            <el-menu-item-group>
+              <template slot="title">Mc</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <template slot="title">An</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group>
+              <template slot="title">Ac</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
 
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
-
-      <el-main>
-        <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140"></el-table-column>
-          <el-table-column prop="name" label="版本名称" width="120"></el-table-column>
-          <el-table-column prop="address" label="版本详情"></el-table-column>
-        </el-table>
-      </el-main>
+      <el-container>
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="日期" width="140"></el-table-column>
+            <el-table-column prop="name" label="版本名称" width="120"></el-table-column>
+            <el-table-column prop="address" label="版本详情"></el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
     </el-container>
   </el-container>
 </template>
@@ -53,6 +55,12 @@
   background-color: #b3c0d1;
   color: #333;
   line-height: 60px;
+}
+
+#title {
+  position: absolute;
+  left: 25px;
+  font-size: 16px;
 }
 
 .el-aside {
