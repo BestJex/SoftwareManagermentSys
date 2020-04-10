57452e43fcad773c99dc866506f2f9d6cdf6fbd3
xiaowu <1181649328@qq.com>
2020-04-10 12:35:10 +0800

    前端配置gzip

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,20 @@
+// 在vue.config.js 中加入
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = ["js", "css"];
+const isProduction = process.env.NODE_ENV === "production";
 module.exports = {
-    lintOnSave: false, //严格模式关闭
-    outputDir: '../server/dist' //每次打包都输出到server目录，作为静态资源
-}
+  lintOnSave: false, //严格模式关闭
+  outputDir: "../server/dist", //每次打包都输出到server目录，作为静态资源
+  configureWebpack: (config) => {
+    if (isProduction) {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          algorithm: "gzip",
+          test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+          threshold: 10240,
+          minRatio: 0.8,
+        })
+      );
+    }
+  },
+};
